7a3f497ddfcbc1a2a4c6fc0ac37fa825b618b18e
Nir Lichtman <nir@lichtman.org>
2025-11-28 22:23:58 +0200

    windows: fix setup script and rm some pkgs

--- setups/windows/setup.cmd
+++ setups/windows/setup.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 REM Vim Text Editor
-winget install vim
+winget install vim.vim
 REM QEMU PC Emulator
 winget install qemu
 REM Microsoft Windows Debugger
@@ -9,12 +9,8 @@ winget install Microsoft.WinDbg
 REM Rizin Reverse Engineering Framework
 winget install Rizin.Rizin
 REM Microsoft Sysinternals
-winget install "Sysinternals Suite"
-REM VirtualBox
-winget install Oracle.VirtualBox
+winget install Microsoft.Sysinternals.Suite
 REM WinDirStat
 winget install WinDirStat.WinDirStat
-REM Bochs PC Emulator
-winget install Bochs.Bochs
 REM 86Box Old School PCs Emulator
 winget install 86Box.86Box
